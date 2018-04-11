a1dacc32fd992389a1e139f15154883e15e71229
Sam <s4perlipopette@gmail.com>
2018-04-11 12:08:53 +0200

    first commit

---
+++ src/main/java/greta/projet_ecole/App.java
@@ -0,0 +1,13 @@
+package greta.projet_ecole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
