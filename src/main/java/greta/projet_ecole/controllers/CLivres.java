6af2a086cce4ec572931a3a2724732c89664393e
Sam <s4perlipopette@gmail.com>
2018-04-11 13:45:07 +0200

    controllers_skeletons

--- src/main/java/greta/projet_ecole/controllers/CLivres.java
+++ src/main/java/greta/projet_ecole/controllers/CLivres.java
@@ -2,4 +2,20 @@ package greta.projet_ecole.controllers;
 
 public class CLivres {
 
+	public void index() {
+
+	}
+
+	public void creerLivre() {
+
+	}
+
+	public void effacerLivre() {
+
+	}
+
+	public void editerLivre() {
+
+	}
+
 }
