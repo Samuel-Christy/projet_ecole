c5b5e209e4f1d838cd48bd4603ec9516b40ab460
Sam <s4perlipopette@gmail.com>
2018-04-11 12:20:24 +0200

    VLivre + hook to main

--- src/main/java/greta/projet_ecole/App.java
+++ src/main/java/greta/projet_ecole/App.java
@@ -1,13 +1,13 @@
 package greta.projet_ecole;
 
+import greta.projet_ecole.views.VLivre;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		VLivre.main();
+	}
 }
